cef8454203d5318b3f010c14e043e623c9efe9fd
Anantha27 <roboticsmind27@gmail.com>
2016-10-29 20:52:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
   user: 'anantha27', //env var: PGUSER
   database: 'anantha27', //env var: PGDATABASE
   password: process.env.DB_PASSWORD, //env var: PGPASSWORD
-  host: 'localhost', // Server hosting the postgres database
+  host: 'db.imad.hasura-app.io', // Server hosting the postgres database
   port: 5432, //env var: PGPORT
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
