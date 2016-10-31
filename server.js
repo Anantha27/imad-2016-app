1d1a8f201f7a6299b8826f9c2abeb7f3d3e4d4d2
Anantha27 <roboticsmind27@gmail.com>
2016-10-31 20:28:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,14 +54,14 @@ app.get('/', function (req, res) {
 app.get('/articles',function(req,res)
 {
     
-   pool.query('SELECT * FROM report',function(err,result){
+   pool.query('SELECT * FROM report',function(err,res){
          if(err)
       {
       res.status(500).send(err.toString());
         }
         else
         {
-            if(result.rows.length === 0){
+            if(res.rows.length === 0){
                  res.status(500).send('Article not found');
             }
             else
