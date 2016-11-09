c9ee3df5e4b96875019681046d00116dcec6aa86
Anantha27 <roboticsmind27@gmail.com>
2016-11-09 12:22:53 +0530

    Delete express1.js

--- express1.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
-
-var server = app.listen(8081)
-console.log("Server Listening on 8081")
