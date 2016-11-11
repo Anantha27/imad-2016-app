e3eb9b0293d622eac5cf9ba206d82e9b0d3a19b8
Anantha27 <roboticsmind27@gmail.com>
2016-11-11 08:17:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/test-db',function(req,res)
 
 });
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'db.js'));
 });
 // create new user
 app.post('/create-user',function(req,res)
