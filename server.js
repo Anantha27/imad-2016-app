f46749dd527a5f264da310ebc68971646a0f5f01
Anantha27 <roboticsmind27@gmail.com>
2016-11-11 18:44:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -177,6 +177,9 @@ app.get('/ui/bootstrap.css', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname,'ui','style.css'));
 });
+app.get('/ui/profile.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
+});
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname,'ui','main.js'));
