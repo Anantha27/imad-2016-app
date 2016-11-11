97aa35ae2ed155c02bf1b695cc131de54d2acbcc
Anantha27 <roboticsmind27@gmail.com>
2016-11-11 08:19:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -159,6 +159,9 @@ app.get('/article-three',function(req,res)
 {
     res.send("Article three will be served here");
 });
+app.get('/ui/db.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'db.js'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
