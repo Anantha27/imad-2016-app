070f257ccb20044f8b42850dc0f917594991b893
Anantha27 <roboticsmind27@gmail.com>
2016-11-12 18:37:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ submit.onclick=function()
 {
     var request=new XMLHttpRequest();
     request.onreadystatechange = function()
-    {if(requet.readyState===XMLHttpRequest.DONE)
+    {if(request.readyState===XMLHttpRequest.DONE)
         {
             if(request.status===200)
             {   console.log('loggedin')
@@ -27,7 +27,7 @@ var username=document.getElementById('username').value;
 var password=document.getElementById('password').value;
 console.log(username);
 console.log(password);
-request.open('POST','http://anantha27.imad.hasura-app.io/login',true);
+request.open("POST","http://anantha27.imad.hasura-app.io/login",true);
 request.setRequestHeader('Content-type','application/json');
 request.send(JSON.stringify({username: username,password: password}));
 };
