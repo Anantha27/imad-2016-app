1673e85559bdf70cf5df86dcf71ac4f2a6cd17f3
Anantha27 <roboticsmind27@gmail.com>
2016-11-12 17:54:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,12 +6,9 @@ var session=require('express-session');
 var app = express();
 var bodyParser=require('body-parser');
 var pg=require('pg');
-var Pool =pg.Pool;
+var Pool =require('pg').Pool;
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ 
-  extended: true
-}));
 
 var config = {
   user: 'anantha27', //env var: PGUSER
