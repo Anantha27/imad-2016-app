4942701b04f9f11724bfaadb395a0118ecc3b6e5
Anantha27 <roboticsmind27@gmail.com>
2016-11-13 07:39:26 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -5,18 +5,18 @@ var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
     var request=new XMLHttpRequest();
-    request.onreadystatechange = function()
-    {if(request.readyState===XMLHttpRequest.DONE)
+    request.onreadystatechange =function()
+    {if(request.readyState === XMLHttpRequest.DONE)
         {
-            if(request.status===200)
+            if(request.status === 200)
             {   console.log('loggedin')
                 alert('Logged in successfully');
             }
-            else if(request.status===403)
+            else if(request.status === 403)
             {
                 alert('Usernmae/password incorrect');
             }
-            else if(request.status===500)
+            else if(request.status === 500)
             {
             alert('Internal Server error');
             }
@@ -28,6 +28,6 @@ var Password=document.getElementById('password').value;
 console.log(userName);
 console.log(Password);
 request.open("POST","http://anantha27.imad.hasura-app.io/login",true);
-request.setRequestHeader('Content-type','application/json');
-request.send(JSON.stringify({userName: userName,Password: Password}));
+request.setRequestHeader('Content-Type','application/json');
+request.send(JSON.stringify({username: userName,password: Password}));
 };
